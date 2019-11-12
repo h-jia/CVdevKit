6af022b75b2974b469cf40d8fb479f22532e8377
Eddy <hj@spaces.ac.cn>
2019-11-12 16:55:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ install_requires = ['torch>=0.4.0', 'Pillow']
 
 setup(
     name='CVdevKit',
-    version='1.0.0.dev1',
-    description='A development kit for Computer Vision',
+    version='1.0.0',
+    description='A development kit for GAN',
     url='https://github.com/JiaminRen/CVdevKit.git',
-    author='Jiamin Ren',
-    author_email='jarman.ren@gmail.com',
+    author='Hong Jia',
+    author_email='h.jia@unsw.edu.au',
     license='Apache',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -22,7 +22,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    keywords='Computer Vision',
+    keywords='GAN Dev',
     packages=find_packages(),
     install_requires=install_requires,
 )
